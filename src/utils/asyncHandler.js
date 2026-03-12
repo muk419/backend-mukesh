8a83dbf2ba8940cd837e8c11bbbefb3718410f19
MAC <mac@Apples-MacBook-Pro.local>
2026-03-12 11:15:24 +0530

    12 march

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+  }
+}
+export { asyncHandler }
+
+// const asyncHandler=(fn)=>{}
+// const asyncHandler=(fn)=>()=>{}
+// const asyncHandler = (fn) => async () => { }
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next)
+//   } catch (error) {
+//     res.status(error.statusCode || 500).json({ success: false, message: error.message, error: error.message })
+//   }
+// }
